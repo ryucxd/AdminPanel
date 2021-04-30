3e52a2672e867503743cf57bab11313758d4bb60
ryucxd <ryucxd@gmail.com>
2021-04-30 13:11:47 +0100

    Add project files.

---
+++ AdminPanel/frmMainMenu.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace AdminPanel
+{
+    partial class frmMainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnOverride = new System.Windows.Forms.Button();
+            this.lblDoors = new System.Windows.Forms.Label();
+            this.lblMen = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblMen);
+            this.groupBox1.Controls.Add(this.lblDoors);
+            this.groupBox1.Controls.Add(this.btnOverride);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(366, 72);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Programming Override";
+            // 
+            // btnOverride
+            // 
+            this.btnOverride.Location = new System.Drawing.Point(6, 29);
+            this.btnOverride.Name = "btnOverride";
+            this.btnOverride.Size = new System.Drawing.Size(83, 23);
+            this.btnOverride.TabIndex = 0;
+            this.btnOverride.Text = "ENABLE";
+            this.btnOverride.UseVisualStyleBackColor = true;
+            this.btnOverride.Click += new System.EventHandler(this.btnOverride_Click);
+            // 
+            // lblDoors
+            // 
+            this.lblDoors.AutoSize = true;
+            this.lblDoors.Location = new System.Drawing.Point(103, 21);
+            this.lblDoors.Name = "lblDoors";
+            this.lblDoors.Size = new System.Drawing.Size(35, 13);
+            this.lblDoors.TabIndex = 1;
+            this.lblDoors.Text = "label1";
+            // 
+            // lblMen
+            // 
+            this.lblMen.AutoSize = true;
+            this.lblMen.Location = new System.Drawing.Point(103, 45);
+            this.lblMen.Name = "lblMen";
+            this.lblMen.Size = new System.Drawing.Size(35, 13);
+            this.lblMen.TabIndex = 2;
+            this.lblMen.Text = "label2";
+            // 
+            // frmMainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 94);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMainMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Admin Panel";
+            this.Load += new System.EventHandler(this.frmMainMenu_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnOverride;
+        private System.Windows.Forms.Label lblMen;
+        private System.Windows.Forms.Label lblDoors;
+    }
+}
+
---
+++ AdminPanel/frmMainMenu.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AdminPanel
+{
+    public partial class frmMainMenu : Form
+    {
+        public frmMainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMainMenu_Load(object sender, EventArgs e)
+        {
+            //load some data 
+            //vv this gets the total doors right now for today
+            string sql = "select COUNT(a.ID) from dbo.door a LEFT OUTER JOIN dbo.door_program AS b ON a.id = b.door_id LEFT OUTER JOIN dbo.door_type AS c ON a.door_type_id = c.id " + //
+                "WHERE(b.programed_by_id IS NULL) AND(a.status_id = 1 OR a.status_id = 2) " +
+                "AND(c.double_y_n IS NOT  NULL) AND(c.slimline_y_n IS NULL OR c.slimline_y_n = 0) AND(a.date_completion IS NOT NULL) AND(a.door_type_id <> 48) AND(a.door_type_id <> 113) " +
+                "AND(a.order_number <> 'Bridge Street Cut Downs') AND a.date_punch <= getdate()";
+            using (SqlConnection conn = new SqlConnection(CONNECT.ConnectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    string getData = Convert.ToString(cmd.ExecuteScalar());
+                    if (getData != null)
+                        lblDoors.Text = "Number of doors to program: " + getData;
+                }
+                sql = "select count(string) from dbo.view_department_reverse_concat_office where placement_date = CAST(GETDATE() as date) and department = 'Programming' and String <> '128'";
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    string getData = Convert.ToString(cmd.ExecuteScalar());
+                    if (getData != null)
+                        lblMen.Text = "Number of programmers currently assigned: " + getData;
+                }
+
+                //here we change button text
+                sql = "SELECT programming_override FROM [user_info].dbo.prog_version_numbers WHERE id = 13";
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    int getData = Convert.ToInt32(cmd.ExecuteScalar());
+                    if (getData == -1)
+                        btnOverride.Text = "DISABLE";
+                    else
+                        btnOverride.Text = "ENABLE";
+                }
+
+                conn.Close();
+            }
+        }
+
+        private void btnOverride_Click(object sender, EventArgs e)
+        {
+            string sql = "";
+            string buttonText = "";
+            if (btnOverride.Text == "ENABLE")
+            {
+                sql = "UPDATE [user_info].dbo.prog_version_numbers SET programming_override = -1 WHERE id = 13";
+                buttonText = "DISABLE";
+            }
+            else
+            {
+                sql = "UPDATE [user_info].dbo.prog_version_numbers SET programming_override = 0 WHERE id = 13";
+                buttonText = "ENABLE";
+            }
+
+            using (SqlConnection conn = new SqlConnection(CONNECT.ConnectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                    btnOverride.Text = buttonText;
+                    if (buttonText != "ENABLE")
+                    {
+                        //fire an email @ gez here
+                            //usp_email_programming_OT
+                            //here we fire the procedure 
+                        using (SqlCommand cmdEmail = new SqlCommand("usp_department_placement_planner_auto_update", conn)
+                            { CommandType = System.Data.CommandType.StoredProcedure })
+                        {
+                            cmdEmail.Parameters.Add("@if_statement", SqlDbType.Int).Value = 4;
+                            cmdEmail.ExecuteNonQuery();
+                        }
+
+                    }
+
+                }
+                conn.Close();
+                Application.Exit();
+            }
+        }
+    }
+}
