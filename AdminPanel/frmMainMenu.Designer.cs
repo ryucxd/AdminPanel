2c36cf728fda79aa5f98eef7b59fa85ea860385f
ryucxd <ryucxd@gmail.com>
2021-05-13 10:14:06 +0100

    added who clicked the button last

--- AdminPanel/frmMainMenu.Designer.cs
+++ AdminPanel/frmMainMenu.Designer.cs
@@ -29,23 +29,43 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.btnOverride = new System.Windows.Forms.Button();
-            this.lblDoors = new System.Windows.Forms.Label();
             this.lblMen = new System.Windows.Forms.Label();
+            this.lblDoors = new System.Windows.Forms.Label();
+            this.btnOverride = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.lblMen);
             this.groupBox1.Controls.Add(this.lblDoors);
             this.groupBox1.Controls.Add(this.btnOverride);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(366, 72);
+            this.groupBox1.Size = new System.Drawing.Size(366, 100);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Programming Override";
+            // 
+            // lblMen
+            // 
+            this.lblMen.AutoSize = true;
+            this.lblMen.Location = new System.Drawing.Point(103, 45);
+            this.lblMen.Name = "lblMen";
+            this.lblMen.Size = new System.Drawing.Size(35, 13);
+            this.lblMen.TabIndex = 2;
+            this.lblMen.Text = "label2";
+            // 
+            // lblDoors
+            // 
+            this.lblDoors.AutoSize = true;
+            this.lblDoors.Location = new System.Drawing.Point(103, 21);
+            this.lblDoors.Name = "lblDoors";
+            this.lblDoors.Size = new System.Drawing.Size(35, 13);
+            this.lblDoors.TabIndex = 1;
+            this.lblDoors.Text = "label1";
             // 
             // btnOverride
             // 
@@ -57,29 +77,23 @@
             this.btnOverride.UseVisualStyleBackColor = true;
             this.btnOverride.Click += new System.EventHandler(this.btnOverride_Click);
             // 
-            // lblDoors
+            // label1
             // 
-            this.lblDoors.AutoSize = true;
-            this.lblDoors.Location = new System.Drawing.Point(103, 21);
-            this.lblDoors.Name = "lblDoors";
-            this.lblDoors.Size = new System.Drawing.Size(35, 13);
-            this.lblDoors.TabIndex = 1;
-            this.lblDoors.Text = "label1";
-            // 
-            // lblMen
-            // 
-            this.lblMen.AutoSize = true;
-            this.lblMen.Location = new System.Drawing.Point(103, 45);
-            this.lblMen.Name = "lblMen";
-            this.lblMen.Size = new System.Drawing.Size(35, 13);
-            this.lblMen.TabIndex = 2;
-            this.lblMen.Text = "label2";
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Brown;
+            this.label1.Location = new System.Drawing.Point(103, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ENABLED 8:29 BY ADAM JENSEN";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Visible = false;
             // 
             // frmMainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(408, 94);
+            this.ClientSize = new System.Drawing.Size(388, 124);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -100,6 +114,7 @@
         private System.Windows.Forms.Button btnOverride;
         private System.Windows.Forms.Label lblMen;
         private System.Windows.Forms.Label lblDoors;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- AdminPanel/frmMainMenu.cs
+++ AdminPanel/frmMainMenu.cs
@@ -13,6 +13,7 @@ namespace AdminPanel
 {
     public partial class frmMainMenu : Form
     {
+        public string _userName { get; set; }
         public frmMainMenu()
         {
             InitializeComponent();
@@ -54,6 +55,22 @@ namespace AdminPanel
                         btnOverride.Text = "ENABLE";
                 }
 
+                //load label text if there is any 
+                sql = "SELECT top 1 * from dbo.programming_permission_log WHERE CAST(time_stamp as date) = CAST(GETDATE() as date) order by id desc";
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    if (dt != null)
+                    {
+                        DateTime temp = Convert.ToDateTime(dt.Rows[0][2].ToString());
+                        string text = dt.Rows[0][3].ToString() + " " + temp.ToString("hh:mm") + " by " + dt.Rows[0][1].ToString();
+                        label1.Text = text;
+                        label1.Visible = true;
+                    }
+                }
+
                 conn.Close();
             }
         }
@@ -66,6 +83,8 @@ namespace AdminPanel
             {
                 sql = "UPDATE [user_info].dbo.prog_version_numbers SET programming_override = -1 WHERE id = 13";
                 buttonText = "DISABLE";
+                //update the  log here
+      
             }
             else
             {
@@ -83,21 +102,64 @@ namespace AdminPanel
                     if (buttonText != "ENABLE")
                     {
                         //fire an email @ gez here
-                            //usp_email_programming_OT
-                            //here we fire the procedure 
+                        //usp_email_programming_OT
+                        //here we fire the procedure 
+
+                        //ALSO INSERT TIMESTAMP OF WHO CLICKED IT HERE
+                        readExcel();
+                        //we got the users id now we can update log table 
+                        //programming_permission_log
+                        sql = "INSERT INTO programming_permission_log (full_name,time_stamp,action) VALUES ('" + _userName + "',GETDATE(),'Enabled')";
+                        using (SqlCommand cmdInsert = new SqlCommand(sql, conn))
+                            cmdInsert.ExecuteNonQuery();
                         using (SqlCommand cmdEmail = new SqlCommand("usp_department_placement_planner_auto_update", conn)
-                            { CommandType = System.Data.CommandType.StoredProcedure })
+                        { CommandType = System.Data.CommandType.StoredProcedure })
                         {
                             cmdEmail.Parameters.Add("@if_statement", SqlDbType.Int).Value = 4;
                             cmdEmail.ExecuteNonQuery();
                         }
-
+                    }
+                    else
+                    {
+                        readExcel();
+                        string sql2 = "INSERT INTO programming_permission_log (full_name,time_stamp,action) VALUES ('" + _userName + "',GETDATE(),'Disabled')";
+                        using (SqlCommand cmdInsert = new SqlCommand(sql2, conn))
+                            cmdInsert.ExecuteNonQuery();
                     }
 
                 }
                 conn.Close();
                 Application.Exit();
             }
+        }
+        private void readExcel()
+        {
+            string file = @"C:\DesignAndSupply_Programs\Session\user_session.csv";
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(file);
+            Microsoft.Office.Interop.Excel.Worksheet xlWorksheet = xlWorkbook.Sheets[1]; // assume it is the first sheet
+            Microsoft.Office.Interop.Excel.Range xlRange = xlWorksheet.UsedRange; // get the entire used range
+             // Microsoft.Office.Interop.Excel.Range range = xlWorksheet.get_Range("A1:A" + last);
+            string temp = xlRange.Cells[2, 1].Value2;
+            xlWorkbook.Close(0);
+            xlApp.Quit();
+            //get full name from this
+            string sql = "SELECT forename + ' ' +surname as [fullName] FROM dbo.[user] WHERE username = '" + temp + "'";
+            using (SqlConnection conn = new SqlConnection(CONNECT.ConnectionStringUser))
+            {
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    conn.Open();
+                   _userName = Convert.ToString(cmd.ExecuteScalar());
+                    conn.Close();
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            readExcel();
         }
     }
 }
